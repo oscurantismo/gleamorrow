4583ea073f91fb39125c7ab1969ec7b9ef800749
oscurantismo <lubyatowo@gmail.com>
2025-06-03 21:53:17 -0400

    Fix color hex codes in styles

---
+++ js/styles.js
@@ -0,0 +1,26 @@
+// styles.js
+
+export const COLORS = {
+    foxyOrange: "#A43612",       // Button and tab backgrounds, badge text
+    deepFurnaceBrown: "#2D0F08",     // Tab/button text and general off-white
+    lighterBrown: "#4E150B",       // Badge background
+    highlightOrange: "#F19043",      // Backgrounds
+    strokeBrown: "#65170A",
+    kindaYellow: "#F5C268",
+};
+
+export const FONT = {
+    heading: "'Negrita Pro', sans-serif",
+    body: "'Reem Kufi Fun', sans-serif",
+};
+
+export const BORDER = {
+    radius: "28px",
+    style: `4px solid ${COLORS.strokeBrown}`,
+};
+
+export const ZINDEX = {
+    topBar: 1000,
+    soundIcon: 1001,
+    navBar: 1000,
+};
